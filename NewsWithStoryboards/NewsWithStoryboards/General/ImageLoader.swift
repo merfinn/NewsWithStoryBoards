d388a0aaa879f7f576c841a11f1e82ff14adc024
Merve KAVAKLIOĞLU <kavaklioglumerve@gmail.com>
2020-05-25 01:19:58 +0200

    Image caching added

---
+++ NewsWithStoryboards/NewsWithStoryboards/General/ImageLoader.swift
@@ -0,0 +1,42 @@
+//
+//  ImageLoader.swift
+//  NewsWithStoryboards
+//
+//  Created by merve kavaklioglu on 25.05.20.
+//  Copyright © 2020 Merve Kavaklioglu. All rights reserved.
+//
+
+import Foundation
+import UIKit.UIImage
+import Combine
+
+public final class ImageLoader {
+    public static let shared = ImageLoader()
+
+    private let cache: ImageCacheType
+    private lazy var backgroundQueue: OperationQueue = {
+        let queue = OperationQueue()
+        queue.maxConcurrentOperationCount = 5
+        return queue
+    }()
+
+    public init(cache: ImageCacheType = ImageCache()) {
+        self.cache = cache
+    }
+
+    public func loadImage(from url: URL) -> AnyPublisher<UIImage?, Never> {
+        if let image = cache[url] {
+            return Just(image).eraseToAnyPublisher()
+        }
+        return URLSession.shared.dataTaskPublisher(for: url) //TODO: connect with url loader with a template function
+            .map { (data, response) -> UIImage? in return UIImage(data: data) }
+            .catch { error in return Just(nil) }
+            .handleEvents(receiveOutput: {[unowned self] image in
+                guard let image = image else { return }
+                self.cache[url] = image
+            })
+            .subscribe(on: backgroundQueue)
+            .receive(on: RunLoop.main)
+            .eraseToAnyPublisher()
+    }
+}
